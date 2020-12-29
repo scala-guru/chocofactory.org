fd26f6b62cfc9c31affb620d3d49ea0e4089fd63
Charlie <chocofactorydev@protonmail.com>
2020-12-29 09:30:40 -0300

    new contracts
    this version correct uniswap treasure contracts address.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7891abc0edac12220105efc2bc9aa7d0.js"
+  "./precache-manifest.2d78a49e7b11fd9d4aea13d956adb4cd.js"
 );
 
 self.addEventListener('message', (event) => {
