0dd7c33a24129e28540cc9fe4e26b04764ea754e
WΞNDΞL <naoesquece@protonmail.com>
2021-01-06 18:05:44 -0300

    publishing the roadmap

--- static/js/7.1099b856.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpChocoFactory=this.webpackJsonpChocoFactory||[]).push([[7],{1079:function(o,c){}}]);
-//# sourceMappingURL=7.1099b856.chunk.js.map
--- static/js/8.043f0d28.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpChocoFactory=this.webpackJsonpChocoFactory||[]).push([[8],{1208:function(o,c){}}]);
-//# sourceMappingURL=8.043f0d28.chunk.js.map
