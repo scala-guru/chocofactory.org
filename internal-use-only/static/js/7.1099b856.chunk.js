81c7bd178665b18eed0f4abed73093273ec63703
WΞNDΞL <naoesquece@protonmail.com>
2021-01-08 15:55:56 -0300

    for internal use only
    - to test pools.
    - to test price display in the app.
    - not for end user.

---
+++ internal-use-only/static/js/7.1099b856.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpChocoFactory=this.webpackJsonpChocoFactory||[]).push([[7],{1079:function(o,c){}}]);
+//# sourceMappingURL=7.1099b856.chunk.js.map
---
+++ internal-use-only/static/js/8.043f0d28.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpChocoFactory=this.webpackJsonpChocoFactory||[]).push([[8],{1208:function(o,c){}}]);
+//# sourceMappingURL=8.043f0d28.chunk.js.map
